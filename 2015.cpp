281f4f36ac4e86b97e74d9cdf2ef62278b4abb77
mrxYan6 <1827732507@qq.com>
2022-09-04 15:28:45 +0800

    9.4

--- 2015.cpp
+++ 2015.cpp
@@ -5,15 +5,13 @@
 
 using ll = long long;
 
-struct edge
-{
-    int v;
-    int wei;
+struct edge {
+    int v, wei;
+
     edge(int v, int w) : v(v), wei(w) {}
 };
 
-int main()
-{
+int main() {
 #ifdef LOCAL
     freopen("in.txt", "r", stdin);
 #endif
@@ -25,8 +23,7 @@ int main()
     int n, q;
     std::cin >> n >> q;
     std::vector<edge> G[n + 1];
-    for (int i = 1; i < n; ++i)
-    {
+    for (int i = 1; i < n; ++i) {
         int u, v, w;
         std::cin >> u >> v >> w;
         G[u].emplace_back(v, w);
@@ -35,25 +32,21 @@ int main()
 
     std::vector<std::vector<int>> dp(n + 1, std::vector<int>(q + 1, 0));
     std::vector<int> size(n + 1, 0);
-    std::function<void(int, int)> dfs = [&](int u, int fa)
-    {
+    std::function<void(int, int)> dfs = [&](int u, int fa) {
         size[u] = 1;
-        for (auto [v, wei] : G[u])
-        {
+        for (auto [v, wei]: G[u]) {
             if (v == fa)
                 continue;
-            dfs(v,u);
+            dfs(v, u);
             size[u] += size[v];
-            for (int i = std::min(q, size[u]); i; --i)
-            {
-                for (int j = std::min(i - 1, size[v]); j >= 0; --j)
-                {
-                    dp[u][i]=std::max(dp[u][i],dp[u][i-j-1]+dp[v][j]+wei);
+            for (int i = std::min(q, size[u]); i; --i) {
+                for (int j = std::min(i - 1, size[v]); j >= 0; --j) {
+                    dp[u][i] = std::max(dp[u][i], dp[u][i - j - 1] + dp[v][j] + wei);
                 }
             }
         }
     };
-    dfs(1,-1);
-    std::cout<<dp[1][q]<<'\n';
+    dfs(1, -1);
+    std::cout << dp[1][q] << '\n';
     return 0;
 }
