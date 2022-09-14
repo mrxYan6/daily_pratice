ca38a8af7dcd475befd7a7b4c85777752ba17c46
mrxYan6 <1827732507@qq.com>
2022-09-14 23:54:48 +0800

    9.15upd

---
+++ UVA11300.cpp
@@ -0,0 +1,19 @@
+//
+// Created by mrx on 2022/9/14.
+//
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+int main() {
+#ifdef LOCAL
+    freopen("in.txt", "r", stdin);
+#endif
+#ifndef LOCAL
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#endif
+
+    return 0;
+}
