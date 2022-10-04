3ce7f191f4a0e85c4b6e73ed37e61105ecdfb41a
mrxYan6 <1827732507@qq.com>
2022-10-04 10:58:58 +0800

    10.4upd

---
+++ UVA11538.cpp
@@ -0,0 +1,18 @@
+//
+// Created by mrx on 2022/10/4.
+//
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <array>
+
+using ll = long long;
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	return 0;
+}
