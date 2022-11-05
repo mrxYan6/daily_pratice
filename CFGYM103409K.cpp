e0ba99d99249fcf7ddee0826d86c066ef4e0d003
mrxYan6 <1827732507@qq.com>
2022-11-05 18:27:41 +0800

    11.5upd

--- CFGYM103409K.cpp
+++ CFGYM103409K.cpp
@@ -20,11 +20,55 @@ int main() {
 
 	int n, m;
 	std::cin >> n >> m;
-	std::vector<std::vector<int>>adj(n);
-	for(int i =0 ;  i <n ;++i){
-
+	std::vector<int> w(m);
+	for (int i = 0; i < m; ++i) {
+		std::cin >> w[i];
 	}
 
+	std::vector<std::vector<std::pair<int, int>>> adj(n);
+
+	for (int i = 0; i < m; ++i) {
+		int u, v, c;
+		std::cin >> u >> v >> c;
+		u--, v--, c--;
+		adj[u].emplace_back(v, c);
+		adj[v].emplace_back(u, c);
+	}
+
+	std::vector<int> dis(n, 0);
+	{
+		std::vector<int> vis(n, false);
+		std::queue<int> q;
+		vis[0] = true;
+		q.push(0);
+		while (!q.empty()) {
+			auto u = q.front();
+			q.pop();
+			for (auto [v, c]: adj[u]) {
+				if (!vis[v]) {
+					dis[v] = dis[u] + 1;
+					vis[v] = true;
+					q.push(v);
+				}
+			}
+		}
+	}
+
+	std::vector<int> cnt(m, 0);
+	std::vector<i64> ans(n, 0x3f3f3f3f);
+	std::function<void(int, i64)> dfs = [&](int u, i64 cost) {
+		ans[u] = std::min(ans[u], cost);
+		for (auto [v, c]: adj[u]) {
+			if (dis[v] == dis[u] + 1) {
+				cnt[c]++;
+				dfs(v, cost + cnt[c] * w[c]);
+				cnt[c]--;
+			}
+		}
+	};
+
+	dfs(0, 0);
+	for (int i = 1; i < n; ++i)std::cout << ans[i] << '\n';
 
 	return 0;
 }
